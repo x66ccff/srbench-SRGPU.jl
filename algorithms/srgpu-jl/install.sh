3ba827d74adef01915d53e95170860b401dd4cc5
x66ccff <2313788275@qq.com>
2025-01-23 12:03:41 +0800

    update install.sh

--- algorithms/srgpu-jl/install.sh
+++ algorithms/srgpu-jl/install.sh
@@ -1,26 +1,16 @@
-# !/bin/bash
-# install ITEA
+#!/bin/bash
 
 # remove directory if it exists
-if [ -d srgpu-jl ]; then
-    rm -rf srgpu-jl
+if [ -d SymbolicRegressionGPU.jl ]; then
+    rm -rf SymbolicRegressionGPU.jl
 fi
 
-# git clone https://github.com/folivetti/tir.git
+# clone repository
 git clone https://github.com/x66ccff/SymbolicRegressionGPU.jl
 
-# cd tir
+# change directory
 cd SymbolicRegressionGPU.jl
-# git checkout 9f51131cabdd52d4dab7a00cbd425bbae05b15b9
 
-chmod +x install.sh
-./install.sh
-
-# WGL NOTE: this is a temp fix until PR https://github.com/folivetti/ITEA/pull/12 is merged
-# install ghcup
-# export BOOTSTRAP_HASKELL_NONINTERACTIVE=1
-# curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | bash
-# export PATH=$PATH:~/.ghcup/bin:~/.cabal/bin 
-
-# #conda activate srbench
-# cabal install --overwrite-policy=always --installdir=./python && cd python && pip install .
+# install and build package
+# julia --project -e 'using Pkg; Pkg.instantiate(); Pkg.build()'
+julia -e 'using Pkg; Pkg.develop(path="."); Pkg.build()'
