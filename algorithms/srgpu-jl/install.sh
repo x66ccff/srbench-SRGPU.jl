11a8872e2483e0520cc6ced51112042fb3913e61
x66ccff <2313788275@qq.com>
2025-01-23 15:27:19 +0800

    update install.sh

--- algorithms/srgpu-jl/install.sh
+++ algorithms/srgpu-jl/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if running in correct environment
+if [ "$CONDA_DEFAULT_ENV" != "srbench-srgpu.jl" ]; then
+    echo "Please run the following commands first:"
+    echo ">>> conda env create -f environment.yml"
+    echo ">>> conda activate srbench-srgpu.jl"
+    echo "Then run this script again."
+    exit 1
+fi
+
 # remove directory if it exists
 if [ -d SymbolicRegressionGPU.jl ]; then
     rm -rf SymbolicRegressionGPU.jl
@@ -12,5 +21,4 @@ git clone https://github.com/x66ccff/SymbolicRegressionGPU.jl
 cd SymbolicRegressionGPU.jl
 
 # install and build package
-# julia --project -e 'using Pkg; Pkg.instantiate(); Pkg.build()'
-julia -e 'using Pkg; Pkg.develop(path="."); Pkg.build()'
+julia --project=. -e 'using Pkg; Pkg.develop(path="."); Pkg.build()'
